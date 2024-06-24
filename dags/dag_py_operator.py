ff8b668559f436b3f4b610730e851db639d5977d
Freanu Peria <magnussonfrancis@gmail.com>
2024-06-25 06:24:43 +0800

    used xcoms for the Python Operators.

--- dags/dag_py_operator.py
+++ dags/dag_py_operator.py
@@ -11,19 +11,30 @@ default_args = {
 }
 
 
-def greet(age, ti):
-    name = ti.xcom_pull(task_ids='get_name')
+def greet(ti):
     print('Running using Python Operator...')
-    print(f'Hello World!!! I am {name} and I am {age} years old')
+
+    first_name = ti.xcom_pull(task_ids='get_name', key='first_name')
+    last_name = ti.xcom_pull(task_ids='get_name', key='last_name')
+    age = ti.xcom_pull(task_ids='get_age', key='age')
+    
+    print(f'Hello World!!! I am {first_name} {last_name} and I am {age} years old')
     print('------------------------------')
 
-def get_name():
-    return 'Games Reid'
+# def get_name():
+#     return 'Games Reid'
 
+def get_name(ti):
+    ti.xcom_push(key='first_name', value='Games')
+    ti.xcom_push(key='last_name', value='Reid')
+    # ti.xcom_push(key='age', value='24')
+
+def get_age(ti):
+    ti.xcom_push(key='age', value='24')
 
 with DAG(
     default_args=default_args,
-    dag_id='dag_py_operator_v04',
+    dag_id='dag_py_operator_v06',
     description='Our first dag using Python Operator',
     start_date=datetime(2024, 6, 24),
     schedule_interval='@daily'
@@ -34,7 +45,7 @@ with DAG(
         python_callable=greet,
         op_kwargs={
             # 'name': 'Games Reid',
-            'age': '24'
+            # 'age': '24'
         }
     )
 
@@ -43,7 +54,12 @@ with DAG(
         python_callable=get_name
     )
 
-    task2 >> task1
+    task3 = PythonOperator(
+        task_id='get_age',
+        python_callable=get_age
+    )
+
+    [task2, task3] >> task1
     # execute task2 first, then, 
     # thru xcoms_pull() inside task1, retrieve return value from task2
 
